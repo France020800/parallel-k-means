ec348bce69e7e9c17a2e1deb3466aba542c2860b
France020800 <francescomarchini7@gmail.com>
2024-02-02 11:57:47 +0100

    Testing on less threads

--- tester.py
+++ tester.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 def run_sequential(random_seed):
     start = time.time()
-    result = subprocess.run(['./bin/k-means', '10', '1000000', str(random_seed)], stdout=subprocess.PIPE, text=True)
+    result = subprocess.run(['./bin/k-means', '10', '100000', str(random_seed)], stdout=subprocess.PIPE, text=True)
     end = time.time()
     output = result.stdout.strip()
     return output, round(end - start, 4)
 
 def run_parallel(random_seed, num_threads):
     start = time.time()
-    result = subprocess.run(['./bin/parallel-k-means', '10',  '1000000', str(random_seed), str(num_threads)], stdout=subprocess.PIPE, text=True)
+    result = subprocess.run(['./bin/parallel-k-means', '10',  '100000', str(random_seed), str(num_threads)], stdout=subprocess.PIPE, text=True)
     end = time.time()
     output = result.stdout.strip()
     return output, round(end - start, 4)
@@ -22,7 +22,7 @@ speedups = []
 # Generate a random seed
 seed = 42
 outS, sequential_time = run_sequential(seed)
-for i in range(1, 17):
+for i in range(1, 10):
     outP, parallel_time = run_parallel(seed, i)
     speedups.append(round(sequential_time/parallel_time, 4))
     print(f'Speedup: {speedups[i-1]}\nThreads number: {i}')
@@ -35,7 +35,8 @@ print("Mean speedup: {}".format(round(sum(speedups) / len(speedups), 4)))
 
 
 # Plot the results
-plt.plot(range(1, 17), speedups, label="Sequential", marker='o')
+plt.plot(range(1, 10), speedups, label="Speedup", marker='o')
+plt.plot(range(1,10), range(1,10), label="Ideal", marker='o', color='coral')
 plt.xlabel('Threads Number')
 plt.ylabel('Speedup')
 plt.title('Speedup vs. Threads Number')
